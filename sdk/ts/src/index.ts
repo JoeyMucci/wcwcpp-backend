214c9c4681ec91e2cb79fc182e078ade0a4f08e0
Joey Mucci <jmucci314@gmail.com>
2026-05-25 23:58:24 -0400

    chore: export all production services in ts sdk

--- sdk/ts/src/index.ts
+++ sdk/ts/src/index.ts
@@ -1,2 +1,19 @@
-export * from "./gen/api/v1/ping_pb";
-export { PingService } from "./gen/api/v1/ping_connect";
+export * from "./gen/api/v1/auth_pb";
+export * from "./gen/api/v1/auth_connect";
+
+export * from "./gen/api/v1/contests_pb";
+export * from "./gen/api/v1/contests_connect";
+
+export * from "./gen/api/v1/leaderboards_pb";
+export * from "./gen/api/v1/leaderboards_connect";
+
+export * from "./gen/api/v1/matches_pb";
+export * from "./gen/api/v1/matches_connect";
+
+export * from "./gen/api/v1/picks_pb";
+export * from "./gen/api/v1/picks_connect";
+
+export * from "./gen/api/v1/types_pb";
+
+export * from "./gen/api/v1/users_pb";
+export * from "./gen/api/v1/users_connect";
